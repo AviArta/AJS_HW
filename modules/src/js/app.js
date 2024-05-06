641aa44fd2857304737bdd449c849f5c5b2ccfc9
AviArta <buzlova.a@yandex.ru>
2024-05-06 11:22:18 +0300

    Add import/export

--- modules/src/js/app.js
+++ modules/src/js/app.js
@@ -1,1 +1,8 @@
+// Модуль App - отвечающий за запуск приложения
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
 console.log('app worked')
+
+const game = new Game();
+game.start();
---
+++ modules/src/js/game.js
@@ -0,0 +1,18 @@
+// Модуль Game - отвечающий за работу приложения (загрузку и сохранение)
+
+import Character from './domain.js';
+
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
